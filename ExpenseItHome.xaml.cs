5c6ad8f1eba53f668dd5f54b5e2f18a053c8c459
eempc <evc484@gmail.com>
2019-03-26 19:36:34 +0000

    halfway replaced inline style with css-like style

---
+++ ExpenseItHome.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// I am using a grid system, similar to CSS Grid. 
+// There are others such as Canvas, DockPanel, StackPanel, Wrap Panel and Virtualizing Stack Panel
+// The XAML defines one column and three rows
+// Then adds a heading label to the top row, a listbox of names to second row and a button to third row
+// Assignments are done with the element's tag
+// This numbering system begins at 0, amazing!
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// So what did I learn eh. Changing title and window dimensions in the XAML file. 
+// Changing from Window to NavigationWindow in the main XAML and upadting the inheritance here.
+// Rerouting the home page, Title Source, weird
+// Adding new pages. So yeah not much has happened at the moment.
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
